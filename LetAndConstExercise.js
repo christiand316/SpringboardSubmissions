f9135620c6a866f41ac2d799da74ec0406e66127
christiand316 <cjd2020hb@gmail.com>
2023-03-24 22:57:49 -0400

    Create LetAndConstExercise.js

---
+++ LetAndConstExercise.js
@@ -0,0 +1,4 @@
+// 1. their scope. let has a block level scope and cannot be redeclared.
+// 2. const cannot be redeclared or have its value changed. let and const cannot be hoisted.
+// 3. let can be redefined.
+// 4. hoisting is when var variables are "initalized" but not defined upon the script starting. this makes it so var variables can be referenced before theyre defined.
