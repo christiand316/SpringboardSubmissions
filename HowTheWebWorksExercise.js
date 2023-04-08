df7675aca096db08498f8c932a2e07dde3fb276a
christiand316 <cjd2020hb@gmail.com>
2023-04-08 09:43:27 -0400

    added howthewebworksexercise

---
+++ HowTheWebWorksExercise.js
@@ -0,0 +1,15 @@
+// part 1
+// 1. http is hyper text transfer protocall, it is a protocall for sending and recieving data.
+// 2. url is uniform resource locator, it is a human readable address for a page.
+// 3. dns is domain name system, it converts urls to ip addresses
+// 4. a query string is information passed along with the url to a server
+// 5. post and get, post sends data to a server while get receives 
+// 6. http requests are a request from a client to a server for information via http
+// 7. http responces are a responce from a server sent to a client
+// 8. headers have more information about a request or response, an example is the language of a page
+// 9. when a url is typed, a dns converts the url to an ip, an http request is sent,an http request is recieved, the browser interprets the data.
+
+// part 2 ... in windows cmd
+// 1. curl -v icanhazdadjoke.com/search/pirate
+// 2. ping icanhazdadjoke.com // ip is 104.21.66.15
+// 3. works 
