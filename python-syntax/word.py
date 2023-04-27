733785e9adc9cc20537ef9a8603aa59aef295dd7
christiand316 <cjd2020hb@gmail.com>
2023-04-27 08:05:51 -0400

    added new exercise

---
+++ python-syntax/word.py
@@ -0,0 +1,11 @@
+def print_upper_words(words)
+    words.upper()
+    if word.startswith('E')
+        for word in words 
+            print(word)
+
+def print_selected_letter(words, start_letter)
+    for word in words
+        if word.startswith(start_letter)
+            print(word.upper())
+            break
