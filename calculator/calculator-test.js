793df72c48dceda23ca858f775139bfe97bed0bf
christiand316 <cjd2020hb@gmail.com>
2023-08-04 19:18:48 -0400

    Update calculator-test.js

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,13 +1,11 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
   let values  = { amount: 50000, years: 6, rate: 7 };
   expect(calculateMonthlyPayment(values)).toBe('852.45');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
   let values  = { amount: 500000, years: 2, rate: 2 };
   expect(calculateMonthlyPayment(values)).toBe('21270.13');
 });
